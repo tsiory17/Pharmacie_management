067c50fe0669f5a1fdf9835d97eaca984410e743
tsiory17 <tsiory.r17@gmail.com>
2025-10-13 23:10:04 -0400

    supplier model

---
+++ Models/Supplier.cs
@@ -0,0 +1,9 @@
+namespace Pharmacie_management.Models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string? SupplierName { get; set; }
+        public string? Phone { get; set; }
+    }
+}
